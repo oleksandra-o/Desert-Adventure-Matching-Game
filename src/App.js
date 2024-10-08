cc501729f932ed7a7fb3139f7da4253d374d35b8
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-10-08 16:23:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import GameBoard from './components/GameBoard';
+import './style.css';
 
 function App() {
+  const [flips, setFlips] = useState(0);
+  const [matches, setMatches] = useState(0);
+
+  const handleReset = () => {
+    setFlips(0);
+    setMatches(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Desert Adventure Match</h1>
       </header>
+      <div id="ui-container">
+        <h3>Flips: {flips}</h3>
+        <h3>Matches: {matches}</h3>
+      </div>
+      <GameBoard flips={flips} setFlips={setFlips} matches={matches} setMatches={setMatches} />
+      <div className="btn-container">
+        <button className="reset-btn" onClick={handleReset}>Reset Game</button>
+      </div>
     </div>
   );
 }
